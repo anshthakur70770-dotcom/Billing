ea0f50acbfd33841e9075d9380481639d222632e
Ansh Thakur <anshthakur70770@gmail.com>
2026-06-18 19:27:18 +0530

    Update page.tsx

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = "force-dynamic";
 import { useLiveQuery } from "dexie-react-hooks";
 import { db } from "@/lib/db";
 import Navbar from "@/components/Navbar";
