4610f5607d018a0e13d99ed2fa9571e2549e3211
Ansh Thakur <anshthakur70770@gmail.com>
2026-06-18 18:50:33 +0530

    Add files via upload

---
+++ lib/db.ts
@@ -0,0 +1,40 @@
+import Dexie, { Table } from "dexie";
+
+export interface Product {
+    id?: number;
+    name: string;
+    stock: number;
+    unit: "piece" | "kg";
+    purchasePrice: number;
+    sellingPrice: number;
+}
+
+export interface Customer {
+    id?: number;
+    name: string;
+    totalSpent: number;
+}
+
+export interface Sale {
+    id?: number;
+    total: number;
+    date: string;
+}
+
+class GroceryDB extends Dexie {
+    products!: Table<Product, number>;
+    customers!: Table<Customer, number>;
+    sales!: Table<Sale, number>;
+
+    constructor() {
+        super("groceryDB");
+
+        this.version(1).stores({
+            products: "++id, name, stock",
+            customers: "++id, name",
+            sales: "++id, date",
+        });
+    }
+}
+
+export const db = new GroceryDB();
