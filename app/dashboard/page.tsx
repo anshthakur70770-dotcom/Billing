df1720f3d8f38bffd638a37ae8b19323faf3e666
Ansh Thakur <anshthakur70770@gmail.com>
2026-06-18 19:28:26 +0530

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = "force-dynamic";
 import BottomNav from "@/components/BottomNav";
 import { useLiveQuery } from "dexie-react-hooks";
 import { db } from "@/lib/db";
